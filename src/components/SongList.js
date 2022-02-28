221ebd93c9cb3af8116b00be4605e98d1908bd40
Guilherme Leite <guilhermelax@hotmail.com>
2022-02-28 10:12:48 -0300

    Feat: implementing addSong on SongList. Installing and implementing uuid

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v1 as uuid } from 'uuid';
 
 const SongList = () => {
+  const [songs, setSongs ] = useState([
+    { title: 'Like acid rain', id: 1 },
+    { title: 'Diamonds', id: 2 },
+    { title: '21', id: 3 },
+  ]);
+  const addSong = () => {
+    setSongs([
+      ...songs,
+      { title: 'new Song', id: uuid() }
+    ]);
+  }
   return ( 
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        { songs.map( (song) => {
+          return (
+            <li key={song.id}>{song.title}</li>
+          )
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 }
